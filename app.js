81c4e15f293ca8ee078b2839147a3df884180c17
Donn Neufeld <69326497+ForeDaddy@users.noreply.github.com>
2022-11-15 09:32:39 -0800

    Returns Rock when Random number is 0

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-function getComputerChoice() {
+function generateRandomNumber() {
+ //Random number between 1 and 3
   return Math.floor(Math.random() * 3);
 }
-console.log(getComputerChoice());
+let computerSelection = generateRandomNumber();
+console.log(computerSelection);
 
-//Random number between 1 and 3
+function getComputerChoice(computerSelection) {
+  if (computerSelection === 0) {
+    return "Rock"
+  } 
+}
+console.log(getComputerChoice(computerSelection));
 //Computer Randomly chooses Rock
 //Or Computer Randomly chooses Paper
 //Or computer Randomly chooses Scissors
