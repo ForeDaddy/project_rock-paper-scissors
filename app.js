74bbeb27d2d56338fa901bbd26dc3d6687f71684
Donn Neufeld <69326497+ForeDaddy@users.noreply.github.com>
2022-11-15 11:52:00 -0800

    Variable computerPlays & playerPlays
    the String value results of player input and computer random selection are assinged to vars comperPlays and playerPlays. These vars will be used in constructing the playGame function.

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const aScissors = "SCISSORS";
 //Get case insensitive user choice
 function getPlayerChoice() {
   let playerChoice = prompt("Choose Rock, Paper, or Scissors", ).toUpperCase();
-  console.log(playerChoice);
+  
  //edge case stop for non valid selection
   if ([aRock, aPaper, aScissors].indexOf(playerChoice) === -1) {
     return "You Have Chosen Poorly!";
@@ -16,14 +16,14 @@ function getPlayerChoice() {
   return playerChoice;
   }
 }
-console.log(getPlayerChoice());
+
 
 function generateRandomNumber() {
  //Random number between 1 and 3
   return Math.floor(Math.random() * 3);
 }
 let computerSelection = generateRandomNumber();
-console.log(computerSelection);
+
 
 function getComputerChoice(computerSelection) {
   //Computer Randomly chooses Rock
@@ -37,5 +37,12 @@ function getComputerChoice(computerSelection) {
     return "SCISSORS"
   } 
 }
-console.log(getComputerChoice(computerSelection));
+//console.log(getComputerChoice(computerSelection));
+//console.log(getPlayerChoice());
 
+let playerPlays = getPlayerChoice();
+console.log(playerPlays);
+let computerPlays = getComputerChoice(computerSelection);
+console.log(computerPlays);
+
+//function playRound()
