053664bf35abafc4ce79c7b580e3b98beedd0d3d
Donn Neufeld <69326497+ForeDaddy@users.noreply.github.com>
2022-11-15 09:17:01 -0800

    Generate random integer 0,1,or 2

---
+++ app.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3);
+}
+console.log(getComputerChoice());
+
+//Random number between 1 and 3
+//Computer Randomly chooses Rock
+//Or Computer Randomly chooses Paper
+//Or computer Randomly chooses Scissors
