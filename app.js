00819179860e290ef249f9b027b6acd577e70a31
Donn Neufeld <69326497+ForeDaddy@users.noreply.github.com>
2022-11-15 09:49:19 -0800

    Prompt for Player choice

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+//Prompt for player choice
+
+
+
+
+
+function getPlayerChoice() {
+  let playerChoice = prompt("Choose Rock, Paper, or Scissors", );
+  return playerChoice;
+}
+console.log(getPlayerChoice());
+
 function generateRandomNumber() {
  //Random number between 1 and 3
   return Math.floor(Math.random() * 3);
@@ -6,15 +18,16 @@ let computerSelection = generateRandomNumber();
 console.log(computerSelection);
 
 function getComputerChoice(computerSelection) {
+  //Computer Randomly chooses Rock
   if (computerSelection === 0) {
     return "Rock"
+  //Or Computer Randomly chooses Paper
   } else if (computerSelection === 1) {
     return "Paper"
+  //Or computer Randomly chooses Scissors
   } else if (computerSelection === 2) {
     return "Scissors"
   } 
 }
 console.log(getComputerChoice(computerSelection));
-//Computer Randomly chooses Rock
-//Or Computer Randomly chooses Paper
-//Or computer Randomly chooses Scissors
+
